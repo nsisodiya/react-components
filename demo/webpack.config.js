007378587e0147a1abb94249cd6fc8af877eecbb
Narendra Sisodiya <narendra@narendrasisodiya.com>
2016-07-20 19:22:55 +0530

    refactor !!

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,62 +2,39 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var LiveReloadPlugin = require("webpack-livereload-plugin");
 var CleanWebpackPlugin = require("clean-webpack-plugin");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
-console.log(__dirname);
+
+var RCRepoDir = __dirname + "/../";
+console.log("RCRepoDir", RCRepoDir);
 module.exports = [{
   cache: true,
-  devtool: "source-map",
+  devtool: "inline-source-map",
   entry: {
-    "ReactComponents": "./src/index.js"
+    "Load": "./Load.js"
   },
   babelrc: false,
   output: {
-    path: "dist",
+    path: "demo-dist",
     filename: "[name].js",
     libraryTarget: "umd",
     library: "[name]"
   },
   plugins: [
-    new ExtractTextPlugin("ReactComponents.css", {
+    new ExtractTextPlugin("Load.css", {
       allChunks: true
     }),
     new LiveReloadPlugin({
-      port: 36001
-    })
+      port: 36002
+    }),
+    new CopyWebpackPlugin([
+      // {output}/file.txt
+      {from: "index.html"}])
   ],
-  externals: {
-    "react": {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
-    },
-    "react-css-modules": {
-      commonjs: "react-css-modules",
-      commonjs2: "react-css-modules",
-      amd: "ReactCSSModules",
-      root: "ReactCSSModules"
-    },
-    "react-bootstrap": {
-      commonjs: "react-bootstrap",
-      commonjs2: "react-bootstrap",
-      amd: "ReactBootstrap",
-      root: "ReactBootstrap"
-    },
-    "uuid": {
-      commonjs: "uuid",
-      commonjs2: "uuid",
-      amd: "UUID",
-      root: "UUID"
-    }
+
+  resolve: {
+    alias: {"@nsisodiya/react-components": RCRepoDir}
   },
+
   module: {
-    preLoaders: [],
     loaders: [
       {
         test: /\.js$/,
@@ -66,16 +43,24 @@ module.exports = [{
           presets: ["es2015", "react"],
           plugins: ["transform-object-rest-spread"]
         }
-      }, {
-        test: /\.css$/,
-        exclude: [/node_modules/],
-        loader: ExtractTextPlugin.extract("style",
-          "css?modules&importLoaders=1&localIdentName=RC_[name]__[local]")
-      }, {
+      },
+      // { test: require.resolve("react"), loader: "expose?React" },
+      // { test: require.resolve("react-dom"), loader: "expose?ReactDOM" },
+      // { test: require.resolve("uuid"), loader: "expose?UUID" },
+      // { test: require.resolve("react-bootstrap"), loader: "expose?ReactBootstrap" },
+      // { test: require.resolve("react-css-modules"), loader: "expose?ReactCSSModules" },
+      {
         test: /\.css$/,
         include: [/node_modules/],
         loaders: ["style", "css"]
-      }, {
+      },
+      {
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        loader: ExtractTextPlugin.extract("style",
+          "css?modules&importLoaders=1&localIdentName=RCDemo_[name]__[local]")
+      },
+      {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
         loader: "url?limit=1000000&mimetype=application/font-woff"
       }, {
